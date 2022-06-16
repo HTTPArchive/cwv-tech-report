83ee936198176e0c3dc0091084ce546936d45bc9
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-06-16 16:47:08 -0400

    Update 02_aggregation.sql

--- opportunities/02_aggregation.sql
+++ opportunities/02_aggregation.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `httparchive.scratchspace.lh_cwv_stats` AS
+CREATE OR REPLACE TABLE `httparchive.core_web_vitals.lighthouse_stats` AS
 SELECT
   percentile,
   technology,
@@ -9,7 +9,7 @@ SELECT
   COUNT(0) AS pages,
   APPROX_QUANTILES(pct_diff, 1000)[OFFSET(percentile * 10)] AS pct_diff
 FROM
-  `httparchive.scratchspace.lh_cwv`,
+  `httparchive.core_web_vitals.lighthouse`,
   UNNEST(GENERATE_ARRAY(1, 100, 1)) AS percentile
 GROUP BY
   percentile,
