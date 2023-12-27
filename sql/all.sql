a776949ebe2a4161208f3f222eaab6b97dfa848c
Rick Viscomi <rviscomi@users.noreply.github.com>
2023-12-27 13:40:03 -0500

    TODOs

--- sql/all.sql
+++ sql/all.sql
@@ -64,11 +64,11 @@ UNION ALL
     IS_GOOD(small_cls, medium_cls, large_cls) AS good_cls,
     IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp) AS any_lcp,
     IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AS good_lcp,
-    
+
     (IS_GOOD(fast_inp, avg_inp, slow_inp) OR fast_inp IS NULL) AND
     IS_GOOD(small_cls, medium_cls, large_cls) AND
     IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AS good_cwv_2024,
-    
+
     (IS_GOOD(fast_fid, avg_fid, slow_fid) OR fast_fid IS NULL) AND
     IS_GOOD(small_cls, medium_cls, large_cls) AND
     IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AS good_cwv_2023,
@@ -163,10 +163,12 @@ SELECT
   COUNTIF(any_fcp) AS origins_with_any_fcp,
   COUNTIF(any_ttfb) AS origins_with_any_ttfb,
   COUNTIF(any_inp) AS origins_with_any_inp,
+  /* TODO(rviscomi): Switch this to `good_cwv_2024` in March 2024. */
   COUNTIF(good_cwv_2023) AS origins_with_good_cwv,
   COUNTIF(good_cwv_2024) AS origins_with_good_cwv_2024,
   COUNTIF(good_cwv_2023) AS origins_with_good_cwv_2023,
   COUNTIF(any_lcp AND any_cls) AS origins_eligible_for_cwv,
+  /* TODO(rviscomi): Switch this to `good_cwv_2024` in March 2024. */
   SAFE_DIVIDE(COUNTIF(good_cwv_2023), COUNTIF(any_lcp AND any_cls)) AS pct_eligible_origins_with_good_cwv,
   SAFE_DIVIDE(COUNTIF(good_cwv_2024), COUNTIF(any_lcp AND any_cls)) AS pct_eligible_origins_with_good_cwv_2024,
   SAFE_DIVIDE(COUNTIF(good_cwv_2023), COUNTIF(any_lcp AND any_cls)) AS pct_eligible_origins_with_good_cwv_2023,
